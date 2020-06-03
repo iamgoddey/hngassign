11d4507333bf4bbee013d1e104a1b89970238924
Godfred Nsabo <godfred@aims.edu.gh>
2020-06-03 01:00:20 +0000

    Add files via upload

---
+++ godfrednsabo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+Full_name = input('Write your full name \n full name: ')
+HNGi7_ID = str(input('Your HNGi7_ID \n HNGi7 ID is '))
+Language = input('What is language \n language: ')
+
+print('Hello World,', 'this is %s' % Full_name,'with HNGi7 ID %s' % HNGi7_ID, 'using %s' % Language)
+
+
+
+
